6844d4610e13a2eb531c944508c72651f18f3bf2
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2021-06-15 18:23:34 +0500

    FlatList added

--- App.js
+++ App.js
@@ -1,32 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
-  const [total, setTotal] = useState(0);
-  const handleIncrementCounter = () => {
-    let result = total + 1
-    setTotal(result);
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (inputTxt) => {
+    setEnteredGoal(inputTxt);
   }
-  const handleDicrementCounter = () => {
-    let result = total - 1
-    setTotal(result);
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ])
+    setEnteredGoal('')
   }
+
   return (
-    <View style={styles.container}>
-      <Button title="Increment!" onPress={() => handleIncrementCounter()} />
-      <Text>{total}</Text>
-      <Button title="Decrement!" onPress={() => handleDicrementCounter()} />
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputPlusBtn}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.inputField}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputPlusBtn: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputField: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
